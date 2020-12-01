abdf6908dcafdc6b0714effbfc8c4e3e20b923f2
bloges56 <brady.drew.logan@gmail.com>
2020-12-01 11:14:45 -0600

    can now add multiple members

--- Member.cs
+++ Member.cs
@@ -8,5 +8,12 @@ namespace Heist
         public string name { get; set; }
         public int skillLevel { get; set; }
         public double courageFactor { get; set; }
+
+        public void displayMember()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine($"Skill Level: {skillLevel}");
+            Console.WriteLine($"Courage Factor: {courageFactor}\n");
+        }
     }
 }
