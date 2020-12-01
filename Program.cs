dc12791d2a68b673ed77aa68ce70004621ecd4f5
bloges56 <brady.drew.logan@gmail.com>
2020-12-01 11:00:51 -0600

    user can now create a member

--- Program.cs
+++ Program.cs
@@ -8,17 +8,19 @@ namespace Heist
         {
             Console.WriteLine("Plan Your Heist!");
 
-            //Declare a member and prompt the user for its properties
+            //Declare a member and prompt the user for its name
             Member userMember = new Member();
             Console.Write("Enter a team member's name: ");
             userMember.name = Console.ReadLine();
-            while(true)
+
+            //prompt the user for the member's skill level
+            while (true)
             {
                 Console.Write("Enter a team member's skill level: ");
                 try
                 {
                     userMember.skillLevel = Int32.Parse(Console.ReadLine());
-                    if(userMember.skillLevel <= 0)
+                    if (userMember.skillLevel <= 0)
                     {
                         throw new Exception();
                     }
@@ -29,6 +31,30 @@ namespace Heist
                     Console.WriteLine("Please enter an integer greater than 0");
                 }
             }
+
+            //prompt the user for the member's courage factor
+            while (true)
+            {
+                Console.Write("Enter a team member's courage factor: ");
+                try
+                {
+                    userMember.courageFactor = Double.Parse(Console.ReadLine());
+                    if (userMember.courageFactor <= 0)
+                    {
+                        throw new Exception();
+                    }
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a decimal value greater than 0");
+                }
+            }
+            
+            //display the member information
+            Console.WriteLine($"Name: {userMember.name}");
+            Console.WriteLine($"Skill Level: {userMember.skillLevel}");
+            Console.WriteLine($"Courage Factor: {userMember.courageFactor}");
         }
     }
 }
