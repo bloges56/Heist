b4befb104e672e14819c3a98d32ae5c66a63b024
bloges56 <brady.drew.logan@gmail.com>
2020-12-01 10:32:04 -0600

    created member class and prompted for name and skill level

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace Heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
+
+            //Declare a member and prompt the user for its properties
+            Member userMember = new Member();
+            Console.Write("Enter a team member's name: ");
+            userMember.name = Console.ReadLine();
+            while(true)
+            {
+                Console.Write("Enter a team member's skill level: ");
+                try
+                {
+                    userMember.skillLevel = Int32.Parse(Console.ReadLine());
+                    if(userMember.skillLevel <= 0)
+                    {
+                        throw new Exception();
+                    }
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an integer greater than 0");
+                }
+            }
         }
     }
 }
